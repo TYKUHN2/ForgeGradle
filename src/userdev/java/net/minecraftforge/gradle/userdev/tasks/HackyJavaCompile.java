6d5e789dc40e7a2f8c929a24543f65c7b6c70e8b
Tyler Kuhn <contact@tyknet.tk>
2020-01-26 16:09:37 -0500

    Removed Backwards Compatibility
    Removed unneeded backwards compatibility in HackyJavaCompiler.

--- src/userdev/java/net/minecraftforge/gradle/userdev/tasks/HackyJavaCompile.java
+++ src/userdev/java/net/minecraftforge/gradle/userdev/tasks/HackyJavaCompile.java
@@ -26,12 +26,9 @@ import org.gradle.api.internal.tasks.compile.DefaultJavaCompileSpec;
 import org.gradle.api.internal.tasks.compile.JavaCompileSpec;
 import org.gradle.api.tasks.WorkResult;
 import org.gradle.api.tasks.compile.JavaCompile;
-import org.gradle.internal.operations.BuildOperationExecutor;
 import org.gradle.jvm.internal.toolchain.JavaToolChainInternal;
 import org.gradle.language.base.internal.compile.Compiler;
 import org.gradle.language.base.internal.compile.CompilerUtil;
-import org.gradle.util.GradleVersion;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -53,46 +50,29 @@ public class HackyJavaCompile extends JavaCompile {
         // Normally, the output history is set by Gradle. Since we're bypassing
         // the normal gradle task infrastructure, we need to do it ourselves.
 
-        // TaskExecutionHistory is removed in 5.1.0, so we only try to set it
-        // on versions below 5.1.0
-
-        if (GradleVersion.current().compareTo(GradleVersion.version("5.1.0")) < 0) {
+        try {
+            Method setPreviousOutputFiles = this.getOutputs().getClass().getMethod("setPreviousOutputFiles", FileCollection.class);
+            setPreviousOutputFiles.invoke(this.getOutputs(), this.getProject().files());
+        } catch (Exception e) {
+            throw new RuntimeException("Exception calling setPreviousOutputFiles ", e);
+        }
+        
+        try {
+            final DefaultJavaCompileSpec spec;
             try {
-                Class<?> taskExectionHistory = Class.forName("org.gradle.api.internal.TaskExecutionHistory");
-                Method setHistory = this.getOutputs().getClass().getMethod("setHistory", taskExectionHistory);
-                Object dummyHistory = Class.forName("net.minecraftforge.gradle.userdev.util.DummyTaskExecutionHistory").newInstance();
-                setHistory.invoke(this.getOutputs(), dummyHistory);
-            } catch (Exception e) {
-                throw new RuntimeException("Exception calling setHistory", e);
+                Method createSpec = JavaCompile.class.getDeclaredMethod("createSpec");
+                createSpec.setAccessible(true);
+                spec = (DefaultJavaCompileSpec) createSpec.invoke(this);
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException("Exception calling createSpec ", e);
             }
-
-            // Do the actual compilation,
-            // bypassing a bunch of Gradle's other stuff (e.g. internal event listener mechanism)
-            this.compile();
-        } else {
-            try {
-                Method setPreviousOutputFiles = this.getOutputs().getClass().getMethod("setPreviousOutputFiles", FileCollection.class);
-                setPreviousOutputFiles.invoke(this.getOutputs(), this.getProject().files());
-            } catch (Exception e) {
-                throw new RuntimeException("Exception calling setPreviousOutputFiles ", e);
-            }
-            try {
-                final DefaultJavaCompileSpec spec;
-                try {
-                    Method createSpec = JavaCompile.class.getDeclaredMethod("createSpec");
-                    createSpec.setAccessible(true);
-                    spec = (DefaultJavaCompileSpec) createSpec.invoke(this);
-                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                    throw new RuntimeException("Exception calling createSpec ", e);
-                }
-                spec.setSourceFiles(getSource());
-                Compiler<JavaCompileSpec> javaCompiler = CompilerUtil.castCompiler(((JavaToolChainInternal) getToolChain()).select(getPlatform()).newCompiler(spec.getClass()));
-                CleaningJavaCompiler compiler = new CleaningJavaCompiler(javaCompiler, getOutputs());
-                final WorkResult execute = compiler.execute(spec);
-                setDidWork(execute.getDidWork());
-            } catch (Exception e) {
-                throw new RuntimeException("Exception compiling ", e);
-            }
+            spec.setSourceFiles(getSource());
+            Compiler<JavaCompileSpec> javaCompiler = CompilerUtil.castCompiler(((JavaToolChainInternal) getToolChain()).select(getPlatform()).newCompiler(spec.getClass()));
+            CleaningJavaCompiler compiler = new CleaningJavaCompiler(javaCompiler, getOutputs());
+            final WorkResult execute = compiler.execute(spec);
+            setDidWork(execute.getDidWork());
+        } catch (Exception e) {
+            throw new RuntimeException("Exception compiling ", e);
         }
 
     }
